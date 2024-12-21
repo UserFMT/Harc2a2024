d92abb3980f20f636634094469ec6d1cf51dce12
UserFMT <fmt147896325@gmail.com>
2024-12-21 22:06:04 +0700

    Update apiPage.py

--- Test_API /apiPage.py
+++ Test_API /apiPage.py
@@ -1,1 +1,17 @@
+import requests
+
+class apiPage:
+
+    def __init__(self, base_url: str) -> None:
+        self.base_url = base_url
+
+
+    def post_list(self, url: str,ss):
+         resp = requests.post(self.base_url+url, headers=ss)
+         return resp
+
+
+    def post_product(self, url, data, ss):
+        resp = requests.post(self.base_url+url, data=data, headers=ss)
+        return resp
 
